30df6abee1356faa7f0984f6cc6d6cdab48e4945
Raul Sanchez Liebana <raul.sanchez.liebana@bbva.com>
2016-07-27 00:46:38 +0200

    Added check for synced if  exists

--- root/opt/monit/bin/monit-start.sh
+++ root/opt/monit/bin/monit-start.sh
@@ -1,1 +1,8 @@
+if [ -d "${SERVICE_VOLUME}" ]; then
+	while [ ! -f ${SERVICE_VOLUME}/.synced ]; do
+		echo `date` $ME - "[ Waiting ${SERVICE_VOLUME} to be synced ... ]"
+		sleep 5
+	done
+fi
+
 /opt/monit/bin/monit -I
